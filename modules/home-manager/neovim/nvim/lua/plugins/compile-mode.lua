2ae5e57d7d3f8ce721784d44448052f56b346146
Adrian Christian Santos <adrian.crsantos@gmail.com>
2026-03-30 18:19:57 +0800

    WIP

--- modules/home-manager/neovim/nvim/lua/plugins/compile-mode.lua
+++ modules/home-manager/neovim/nvim/lua/plugins/compile-mode.lua
@@ -1,6 +1,7 @@
 return {
   "ej-shafran/compile-mode.nvim",
+  ft = { "cpp" },
   dependencies = {
     "nvim-lua/plenary.nvim",
-  };
+  },
 }
--- modules/home-manager/neovim/nvim/lua/plugins/lazyvim.lua
+++ modules/home-manager/neovim/nvim/lua/plugins/lazyvim.lua
@@ -5,7 +5,7 @@ return {
     lazy = false,
     opts = {
       dashboard = {
-        enabled = true,
+        enabled = false,
         preset = {
           header = [[
 ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗
